d189825425e8f9a180f77d0fe59f97a5033b60ee
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2025-02-13 13:22:21 +0000

    uniformity test

--- src/uniformity_test.jl
+++ src/uniformity_test.jl
@@ -5,9 +5,9 @@ Performs a chi-squared test for uniformity on the given `counts` vector given `n
 - A `HypothesisTests.PowerDivergenceTest` object representing the result of the chi-squared test.
 """
 function uniformity_test(counts::Vector{Int}, n::Int)
-    contingency_table = zeros(Int, n+1)
+    contingency_table = zeros(Int, n + 1)
     for val in counts
-        contingency_table[val+1] += 1
+        contingency_table[val + 1] += 1
     end
     ChisqTest(contingency_table) #Default is to compare to uniform distribution
 end
--- test/uniformity_test.jl
+++ test/uniformity_test.jl
@@ -1,4 +1,19 @@
 @testitem "Test uniformity test" begin
-    using Distributions, HypothesisTests, Random
+    using Distributions, Random
+    using HypothesisTests: pvalue
+    Random.seed!(1234)
 
+    n = 100 # number of samples pre comparison
+    n_comparisons = 1000 # number of trial comparisons
+
+    # This is the target discrete uniform distribution
+    good_counts = rand(0:n, n_comparisons)
+    # This is not a uniform distribution
+    bad_counts = rand(Poisson(n / 2), n_comparisons)
+
+    # Test the good distribution
+    good_t = chisqtest(good_counts)
+    @test pvalue(good_t) > 0.05
+    bad_t = chisqtest(bad_counts)
+    @test pvalue(bad_t) < 0.05
 end
