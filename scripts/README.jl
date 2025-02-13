d2ea7ee0fc64e6e5a55e563c8ce81b079d6e62d0
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2025-02-13 15:13:44 +0000

    New Readme

---
+++ scripts/README.jl
@@ -0,0 +1,56 @@
+# # SBC
+# [![Aqua QA](https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg)](https://github.com/JuliaTesting/Aqua.jl)
+
+# This package provides tools for performing simulation-based calibration (SBC) on generative models.
+# Out-of-the-box, it provides tools for comparing generative univariate distributions, but
+# it can be extended to other types of random sampling, namely full simulation based calibration of `Turing.jl` models.
+
+# Extensions have to comply with the `SBCInterface` provided by `SBC.jl` using `Interfaces.jl`.
+# At the moment this is very lightweight:
+# Required methods:
+# - `run_primary_generative` this samples from the primary distribution and returns a tuple of the sample from the target distribution and the auxiliary variable.
+# - `run_secondary_generative` this samples from the secondary distribution `n` times possibly conditional the auxiliary variable.
+# - `run_comparison`: This runs both the methods above sequentially `n_comparison` times, and gathers the rank statistics. By default these get
+# passed to the `uniformity_test` function to return a Chi-squared test result.
+
+# ## Example
+# In this example, we define a form of Normal distribution that has no distributional knowledge,
+# but can be sampled from. We make this a subtype of the `Distributions.Sampleable` type and provide
+# the necessary methods.
+
+using SBC, Distributions, HypothesisTests, Random
+Random.seed!(1234)
+
+struct SampleNormal{T} <: Sampleable{Univariate, Continuous} where {T <: Real}
+    μ::T
+    σ::T
+end
+
+function Base.rand(rng::AbstractRNG, s::SampleNormal)
+    return s.μ + s.σ * randn()
+end
+
+function Random.rand!(rng::AbstractRNG, s::SampleNormal, x::Vector)
+    ϵ = randn(rng, length(x))
+    @. x = s.μ + s.σ * ϵ
+    return x
+end
+
+# Now we can define a `CompareDistGenerator` that will compare two `SampleNormal` distributions.
+# As a first pass lets just compare two normal distributions with the same mean and variance.
+
+compare_dists = CompareDistGenerator(SampleNormal(0.0, 1.0), SampleNormal(0.0, 1.0))
+n = 100
+n_comparisons = 10_000
+results = run_comparison(compare_dists, n, n_comparisons)
+results.test_results
+
+# We see that there is no evidence that the two distributions are different.
+# By contrast is we test whether the sampling distribution is different from a Cauchy distribution
+# with scale 1.0.
+
+compare_different_dists = CompareDistGenerator(SampleNormal(0.0, 1.0), Cauchy(1.0))
+results_diff = run_comparison(compare_different_dists, n, n_comparisons)
+results_diff.test_results
+
+# We do indeed see strong evidence that the two distributions are different.
