54eec7d3169fa3eb4f2d26152554da3563824a07
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2025-02-25 12:18:47 +0000

    Fix for array shaped dists (#12)

--- test/ext/TuringExt/test_models.jl
+++ test/ext/TuringExt/test_models.jl
@@ -1,8 +1,23 @@
+# Model with only scalar distributions
 @testsnippet TestModel1 begin
     using Turing
     @model function test_model()
         mu ~ Normal(0, 1)
         x ~ Normal(mu, 1)
+        return x
+    end
+    sampler = NUTS()
+    condition_names = (:x,)
+    turing_model = test_model()
+end
+
+# Model with array distribution
+@testsnippet TestModel2 begin
+    using Turing
+    nd = 10
+    @model function test_model()
+        mu ~ filldist(Normal(0, 1), nd)
+        x ~ arraydist([Normal(m, 1) for m in mu])
         return x
     end
     sampler = NUTS()
